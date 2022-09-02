61747d64e4f7e5043765ade4f67cf6813e1e7e1a
Oluwaseun <motdde@users.noreply.github.com>
2022-09-02 21:13:59 +0100

    chore: run k8's script

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -2,17 +2,14 @@
 
 # This tags and uploads an image to Docker Hub
 
-# Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=motdde/cloud-devops-capstone
 
-# Step 2
 # Run the Docker Hub container with kubernetes
+kubectl run capstone-devops --image=$dockerpath
 
-
-# Step 3:
 # List kubernetes pods
+kubectl get pods
 
-# Step 4:
 # Forward the container port to a host
-
+kubectl port-forward capstone-devops 8000:80
